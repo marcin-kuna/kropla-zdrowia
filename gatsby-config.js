fd77d25f05bc23a7a632d9af46339cd431bb5feb
Marcin Kuna <marcin.m.kuna@gmail.com>
2020-10-03 23:33:42 +0200

    Viewport RWD

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-smoothscroll`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-scroll-reveal`,
       options: {
