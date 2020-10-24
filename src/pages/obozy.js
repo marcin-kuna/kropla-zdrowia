8c35247e460f6d5b2a324e9d3d085715f4029995
Marcin Kuna <marcin.m.kuna@gmail.com>
2020-10-24 23:45:25 +0200

    Camps styling - WIP

--- src/pages/obozy.js
+++ src/pages/obozy.js
@@ -7,6 +7,15 @@ import WaveSeparator from "../assets/images/wave-long.svg"
 import WaveSeparatorYellow from "../assets/images/wave-long-yellow.svg"
 import "../styles/obozy.scss"
 import scrollTo from "gatsby-plugin-smoothscroll"
+import { MdLocationOn, MdSchedule, MdRestaurant, MdCheck } from "react-icons/md"
+import {
+  FaHome,
+  FaRegMoneyBillAlt,
+  FaBusAlt,
+  FaRegQuestionCircle,
+} from "react-icons/fa"
+import { AiOutlineSchedule } from "react-icons/ai"
+import CampsWave from "../assets/images/campsWave.svg"
 
 const query = graphql`
   {
@@ -71,6 +80,7 @@ const Obozy = () => {
         <h2 className="camps-title section-title">
           Obozy sportowe i rekreacyjne
         </h2>
+
         <div className="camps-nav">
           {camps.map((item) => {
             return (
@@ -97,12 +107,15 @@ const Obozy = () => {
               </button>
             )
           })}
+          <img src={CampsWave} alt="" className="camps-wave camps-wave-nav" />
         </div>
+
         <section className="camps-container">
           {camps.map((item) => {
             return (
               <div
-                className="camp-container"
+                // className="camp-container"
+                className="camp"
                 key={item.contentfulid}
                 id={item.selector}
               >
@@ -112,60 +125,104 @@ const Obozy = () => {
                   alt=""
                   className="camp-separator"
                 />
-                <div
-                  className="camp"
-                  key={item.contentfulid}
-                  id={item.selector}
-                >
+                {/* <div className="camp"> */}
+                <div className="camp-info">
                   <Image
                     fluid={item.image.fluid}
                     className="camp-image"
                     alt=""
                   />
-
-                  <div className="camp-info">
-                    <p className="camp-description">
-                      {item.description.description}
-                    </p>
-                    <h3>Program</h3>
-                    <div className="camp-program">
-                      {item.program.content.map((element, index) => {
-                        return <p key={index}>{element.content[0].value}</p>
-                      })}
-                    </div>
-                    <h3>Termin</h3>
+                  <p className="camp-description">
+                    {item.description.description}
+                  </p>
+                </div>
+                <h3>
+                  <AiOutlineSchedule className="camp-details-icon" />
+                  Program
+                </h3>
+                <div className="camp-program">
+                  {item.program.content.map((element, index) => {
+                    return (
+                      <p key={index}>
+                        <MdCheck className="camp-details-icon" />
+                        {element.content[0].value}
+                      </p>
+                    )
+                  })}
+                </div>
+                <div className="camp-details">
+                  <div className="camp-details-container">
+                    <h3>
+                      <MdSchedule className="camp-details-icon" />
+                      Termin
+                    </h3>
                     <p className="camp-date">{item.date}</p>
-                    <h3>Miejsce</h3>
+                  </div>
+                  <div className="camp-details-container">
+                    <h3>
+                      <MdLocationOn className="camp-details-icon" />
+                      Miejsce
+                    </h3>
                     <p className="camp-location">{item.location}</p>
-                    <h3>Zakwaterowanie</h3>
+                  </div>
+                  <div className="camp-details-container">
+                    <h3>
+                      <FaHome className="camp-details-icon" />
+                      Zakwaterowanie
+                    </h3>
                     <p className="camp-accomodation">{item.accommodation}</p>
-                    <h3>Wyżywienie</h3>
+                  </div>
+                  <div className="camp-details-container">
+                    <h3>
+                      <MdRestaurant className="camp-details-icon" />
+                      Wyżywienie
+                    </h3>
                     <p className="camp-food">{item.food}</p>
-                    <h3>Transport</h3>
+                  </div>
+                  <div className="camp-details-container">
+                    <h3>
+                      <FaBusAlt className="camp-details-icon" />
+                      Transport
+                    </h3>
                     <p className="camp-transport">{item.transport}</p>
-                    <h3>Cena</h3>
+                  </div>
+                  <div className="camp-details-container">
+                    <h3>
+                      <FaRegMoneyBillAlt className="camp-details-icon" />
+                      Cena
+                    </h3>
                     <p className="camp-price">{item.price}</p>
-                    <h3>Dostępność</h3>
-                    <div className="camp-availability">
-                      {item.availability.content.map((element, index) => {
-                        return (
-                          <p
-                            key={index}
-                            className={`${
-                              element.content[0].value === "wolne miejsca"
-                                ? `green`
-                                : element.content[0].value === "brak miejsc"
-                                ? `red`
-                                : ""
-                            }`}
-                          >
-                            {element.content[0].value}
-                          </p>
-                        )
-                      })}
-                    </div>
                   </div>
                 </div>
+                <h3>
+                  <FaRegQuestionCircle className="camp-details-icon" />
+                  Dostępność
+                </h3>
+                <div className="camp-availability">
+                  {item.availability.content.map((element, index) => {
+                    return (
+                      <p
+                        key={index}
+                        className={`${
+                          element.content[0].value === "wolne miejsca"
+                            ? `green`
+                            : element.content[0].value === "brak miejsc"
+                            ? `red`
+                            : ""
+                        }`}
+                      >
+                        {element.content[0].value}
+                      </p>
+                    )
+                  })}
+                </div>
+
+                {/* </div> */}
+                <img
+                  src={CampsWave}
+                  alt=""
+                  className="camps-wave camps-wave-dark"
+                />
               </div>
             )
           })}
