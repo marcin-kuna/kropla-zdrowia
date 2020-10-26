43cdf483de4073a44c55aa46221fc3087c92a8fe
Marcin Kuna <marcin.m.kuna@gmail.com>
2020-10-26 23:19:18 +0100

    Contact basic structure & styling

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -2,6 +2,10 @@ import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/Layout"
 import SubpageLayout from "../components/SubpageLayout"
+import "../styles/kontakt.scss"
+import SocialLinks from "../constants/socialLinks"
+import { MdLocationOn, MdEmail } from "react-icons/md"
+import { FaPhone, FaPiggyBank } from "react-icons/fa"
 
 const query = graphql`
   {
@@ -25,21 +29,50 @@ const Kontakt = () => {
   return (
     <Layout>
       <SubpageLayout image={fluid} heading="Skontaktuj się!">
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est fugit
-          consectetur dolore ducimus repellendus, nam nihil nulla reprehenderit
-          unde quibusdam officia? Aliquid ex, dignissimos quas accusamus
-          delectus pariatur veritatis fugiat laudantium vel illum recusandae
-          ipsam? Aliquam velit atque eveniet cupiditate asperiores blanditiis
-          sequi ut provident dolore fugit suscipit explicabo perspiciatis illo
-          dolorem ipsa laboriosam quos corrupti, possimus alias veniam
-          recusandae expedita mollitia doloremque. Accusamus quidem minima
-          consequatur ut ducimus, modi ipsum optio veritatis doloremque
-          similique aspernatur dolorum labore quis expedita, repudiandae, sed
-          quod officiis sapiente? Consectetur necessitatibus quae, repudiandae
-          laborum culpa distinctio sint deserunt esse labore cum similique,
-          ipsam explicabo?
-        </div>
+        <section className="contact">
+          <div className="contact-info">
+            <h2>Kropla Zdrowia</h2>
+            <div className="contact-info-details">
+              <p>
+                <MdLocationOn className="contact-info-icon" />
+                ul. Focha 13/21, 26-600 Radom
+              </p>
+              <p>
+                <FaPhone className="contact-info-icon" />
+                (+48) 603 952 902
+              </p>
+              <p>
+                <MdEmail className="contact-info-icon" />
+                email@email.com
+              </p>
+              <p>
+                <FaPiggyBank className="contact-info-icon" />
+                Bank: 84 2490 0005 0000 4600 6531 1730
+              </p>
+            </div>
+            <SocialLinks />
+          </div>
+          <div class="form-wrapper">
+            <form name="contact" method="POST" data-netlify="true">
+              <div class="formgroup" id="name-form">
+                <label htmlFor="name">Twoje imię</label>
+                <input type="text" id="name" name="name" />
+              </div>
+
+              <div class="formgroup" id="email-form">
+                <label htmlFor="email">Twój e-mail</label>
+                <input type="email" id="email" name="email" />
+              </div>
+
+              <div class="formgroup" id="message-form">
+                <label htmlFor="message">Twoja wiadomość</label>
+                <textarea id="message" name="message"></textarea>
+              </div>
+
+              <input type="submit" value="Wyślij!" />
+            </form>
+          </div>
+        </section>
       </SubpageLayout>
     </Layout>
   )
